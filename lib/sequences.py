dae94f49150050a036ad266ffd0e02c9b32b38c2
lucky wangie <wangiemamati@gmail.com>
2025-02-13 12:10:51 +0300

    tests passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    a, b = 0, 1
+    while len(fib_sequence) < length:
+        fib_sequence.append(a)
+        a, b = b, a + b
+    print(fib_sequence)
+
+
+
+# function test
+print_fibonacci(9)
